67803349c2ac3afd64ee9ba2b3ea47704a9e3f05
stevenseand <stevenseand@gmail.com>
2015-06-20 16:43:38 +0700

    Update square.h

--- square.h
+++ square.h
@@ -1,21 +1,31 @@
 #ifndef Square_H
 #define Square_H
-using namespace std;
-#include <string>
-#include <vector>
 
-class Square: public Shape
-{
-	public:
-	string s;
-	void loadFromFile(char *filename);
-	void saveToFile(char *filename);
-	Square(string s);
-	Square();
-	~Square();
-	vector<int>ukuran;
-	void printAll();
-	int getSize();
-	void sort();
+#include "shape.h"
+
+
+class Square : public Shape {
+    private:
+        int sisi;
+
+    public:
+		void loadFromFile(char *filename);
+		void saveToFile(char *filename);
+		void Luaskotak();
+		void Kelilingkotak();
+		void Tambah(double a);
+		void Kurang(double a);
+		Square();
+		~Square();
+		void sorting();
+       Square (int sisi) : sisi(sisi) {}
+
+        virtual int getArea() const {
+            return sisi*sisi;
+        };
+		
+        virtual int setSisi(int sisi){
+            sisi = sisi;
+        }
 };
 #endif

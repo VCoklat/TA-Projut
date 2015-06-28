1fba7da31e2732b1728552f87779b09bb51052dd
stevenseand <stevenseand@gmail.com>
2015-06-28 11:10:57 +0700

    Update square.cpp

--- square.cpp
+++ square.cpp
@@ -1,77 +1,122 @@
+#include<iostream>
 #include "square.h"
+#include "shape.h"
 
-//Constructor
-Square::Square(){
+using namespace std;
+
+//vector <Shape *> shapes;
+
+Square::Square()
+{
+	sisisquare=0;
+	jenisShape="Square";
+	hitungLuas();
+	hitungKeliling();
 	loadFromFile((char *)"square.txt");
-	//Luas();
 }
 
-//Destructors
 Square::~Square(){
 	saveToFile((char *)"outsquare.txt");
 }
 
 void Square::loadFromFile(char *filename)
 {
-	int s;
-	FILE* file = fopen (filename, "s"); //baca file
+	int si; //si = sisi
+	FILE* file = fopen (filename, "si"); //baca file
 		while(!feof (file))
 		{
-			fscanf (file, "%d", &s); 
-			jariList.push_back(s); // memasukkan data ke dalam vektor
+			fscanf (file, "%d", &si); 
+			shapes.push_back(new Square(si)); // memasukkan data ke dalam vektor
 		}	
-	sorting();
 	fclose (file);    
 }
 
-void Square::sorting()
+Square::Square(int sisiBaru)
 {
-	std::sort(sisiList.begin(),sisiList.end());//untuk sorting
+	sisi=sisiBaru;
+	jenisShape="Square";
+	hitungLuas();
+	hitungKeliling();
+} 
+
+void Square::hitungLuas()
+{
+	luas=sisi*sisi;
 }
 
-void Square::Tambah(double a)
+void Square::hitungKeliling()
 {
-	sisiList.push_back(a);
+	keliling=4*sisi;	
 }
 
-void Square::Kurang(double a)
+void Square::printDetails()
+{
+	cout<<sisi<<" | "<<luas<<" | "<<keliling<<endl;
+}
+
+void Square::tambah(int a)
+{
+	if (a>0) shapes.push_back(new Square(a)); else throw"Input tidak boleh negatif";
+}
+
+void Square::hapus(int a)
+{
+	bool cek = false;
+	if (a<0) throw"Input tidak boleh negatif";
+	for (int i=0;i<shapes.size();i++)
+	{
+		if(shapes[i]->getJenisShape()=="Square")
+		{
+			if(((Square*)shapes[i])->getsisi() == a)
+			{
+				cek = true;
+				shapes.erase(shapes.begin()+i);
+				cout<<"Square dengan sisi "<<a<<" telah di hapus"<<endl;
+			}
+		}
+	}	
+	if(!cek)
+	{
+		throw"Tidak ada persegi dengan sisi tersebut.";
+	}	
+}
+
+void Square::tampilluas()
 {
 	int i=0;
-	while(i<sisiList.size())
+	sort(shapes.begin(),shapes.begin()+shapes.size(),Shape::sortByLuas);
+	cout<<"Sisi | Luas | Keliling "<<endl;
+	for(i=0;i<shapes.size();i++)
 	{
-		if (a==sisiList[i])
-		{
-			sisiList.erase(sisiList.begin()+i);
-			i--;
-		}
-		i++;
+		if(shapes[i]->getJenisShape()=="Square") shapes[i]->printDetails();
 	}
 }
 
-void Square::Luas()
-{	
-	for(int i=0;i<sisiList.size();i++)
+void Square::tampilkeliling()
+{
+	int i=0;
+	sort(shapes.begin(),shapes.begin()+shapes.size(),Shape::sortByKeliling);
+	cout<<"Sisi | Luas | Keliling "<<endl;
+	for(i=0;i<shapes.size();i++)
 	{
-		Asquare.push_back(sisiList[i]*sisiList[i]);
-		cout<<"Sisi : "<<sisiList[i]<<" luas : "<<Asquare[i]<<endl; //output sisi & luas
+		if(shapes[i]->getJenisShape()=="Square") shapes[i]->printDetails();
 	}
 }
 
-void Square::Keliling()
-{	
-	for(int i=0;i<sisiList.size();i++)
-	{
-		Ksquare.push_back(sisiList[i]*4);
-		cout<<"Sisi : "<<sisiList[i]<<" Keliling : "<<Ksquare[i]<<endl;
-	}
+int Square :: getsisi()
+{
+	return sisi;
 }
 
 void Square::saveToFile(char *filename)
 {
 	ofstream save (filename);
-	for (int i=0;i<sisiList.size();i++)
+	for(int i=0;i<shapes.size();i++)
 	{
-		save<<sisiList[i]<<endl; //save data
+		if(shapes[i]->getJenisShape()=="Square")
+		{
+			save<<((Square *)shapes[i])->getsisi()<<endl;
+		}
 	}
 	save.close();
 }

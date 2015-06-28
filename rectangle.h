95841edf2d3a64db7fadcd6812ac08add9e30838
stevenseand <stevenseand@gmail.com>
2015-06-28 12:58:00 +0700

    Tolong dicek kalo ada salah ya..

---
+++ rectangle.h
@@ -0,0 +1,28 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+#include "shape.h"
+
+using namespace std;
+
+class Rectangle : public Shape
+{
+	public:
+		Rectangle();
+		void loadFromFile(char *filename);
+		void tampilluas();
+		void tampilkeliling();
+		void tambah(int a, int b);
+		void hapus(int a, int b);
+		~Rectangle();
+		void saveToFile(char *filename);
+		Rectangle(int panjangBaru, int lebarBaru);
+		void hitungLuas();
+		void hitungKeliling();
+		void printDetails();
+		int getpanjang();
+		int getlebar();
+	protected:
+		int panjang;
+		int lebar;
+};
+#endif
